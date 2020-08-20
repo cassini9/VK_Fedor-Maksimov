97814709b8127820154504bfd5d4ba029d43a732
Fedor <fedormaks@gmail.com>
2020-08-20 21:20:28 +0300

    Lesson

---
+++ VK_Fedor Maksimov/LoginFromController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginFromController.swift
+//  VK_Fedor Maksimov
+//
+//  Created by Fedor Maksimov on 20.08.2020.
+//  Copyright © 2020 Fedor Maksimov. All rights reserved.
+//
+
+import UIKit
+
+class LoginFromController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
